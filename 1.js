b258fe13fd1b10050941aae45b87e3d4b3d82e94
fikriansyah-31 <syafanurwalinda23@gmail.com>
2022-07-13 13:57:38 +0700

    ...

--- 1.js
+++ 1.js
@@ -1,14 +1,34 @@
-// function bilanganPrima(prima) {
-//     let bilanganPrima = 1
-//     for (i = 2; i<= (prima / 2); i++) {
-//         if (prima % i == 0) {
-//             bilanganPrima = 0;
-//             break;
-//         }
-//     }
-//     if(bilanganPrima == 1 || prima == 2)
-//         return true;
-//     else
-//         return false;
-        
-// }
+function isPrimeNum(prime) {
+    let isPrime = true;
+    for (i = 2; i <= (prime / 2); i++) {
+        if (prime % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime == 1 || prime == 2)
+        return true;
+    else
+        return false;
+}
+
+function drawSegitiSiku(length) {
+    if (length <= 0 || length >= 10) {
+        return console.log("note: length input must 0 < alas/tinggi < 10")
+    } else {
+        let prime = 2;
+        for (let i = 1; i <= length; i++) {
+            let s = ""
+            for (let j = 1; j <= i; j++) {
+                while (!isPrimeNum(prime)) {
+                    prime++;
+                }
+                s += prime++ + " ";
+            }
+            console.log(s);
+        }
+        return (false);
+    }
+}
+
+drawSegitiSiku(7)
